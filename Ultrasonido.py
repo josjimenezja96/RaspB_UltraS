ca79bfe1d5acb91494ea7e23d7c14cbc0a81148a
Joan Sebastian Jimenez Jaimes <76706326+josjimenezja96@users.noreply.github.com>
2022-02-22 18:29:27 -0500

    Update Ultrasonido.py

--- Ultrasonido.py
+++ Ultrasonido.py
@@ -16,13 +16,18 @@ GPIO.output(TRIG, True)
 time.sleep(0.00001)
 GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO) == 0:
-        pulse_start = time.time()
-while GPIO.input(ECHO) == 1:
-        pulse_end = time.time()
+def Distance():
 
-pulse_duration = pulse_end - pulse_start
+        while GPIO.input(ECHO) == 0:
+                pulse_start = time.time()
+        while GPIO.input(ECHO) == 1:
+                pulse_end = time.time()
 
-distance = pulse_duration * 17150
-distance = round(distance,2)
-print("Distance: ",distance,"cm)
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance,2)
+        return distance
+
+while True:
+        Distance()
+        time.sleep(1)
