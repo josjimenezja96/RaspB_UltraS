2fbd94708af42f116b84a73a0b1fc6c096c85791
Joan Sebastian Jimenez Jaimes <76706326+josjimenezja96@users.noreply.github.com>
2022-02-22 18:39:42 -0500

    Update Ultrasonido.py

--- Ultrasonido.py
+++ Ultrasonido.py
@@ -1,22 +1,30 @@
 import RPi.GPIO as GPIO
 import time
+
+# GPIO Mode (Board / BCM)
 GPIO.setmode(GPIO.BCM)
 
+# DEFINE PINS
 TRIG = 23
 ECHO = 24
 
+# SETUP PINS
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
-GPIO.output(TRIG, False)
-print("Waitin For sensor to settle")
-time.sleep(2)
-
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
 
 def Distance():
+        
+        #GPIO.output(TRIG, False)
+        #print("Waitin For sensor to settle")
+        #time.sleep(2)
+        
+        # Set trig to True
+        GPIO.output(TRIG, True)
+        
+        # Set trig after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
 
         while GPIO.input(ECHO) == 0:
                 pulse_start = time.time()
@@ -25,12 +33,17 @@ def Distance():
 
         pulse_duration = pulse_end - pulse_start
         distance = pulse_duration * 17150
-        distance = round(distance,2)
-        GPIO.cleanup()
-        
+        distance = round(distance, 2)
         return distance
 
-while True:
-        X = Distance()
-        print("Distance: ", X , " cm")
-        time.sleep(1)
+if __name__ == '__main':
+        try:
+                while True:
+                        dist = Distance()
+                        print("Distance: %0.1f" % dist)
+                        time.sleep(0.5)
+        except KeyboardInterrupt:
+                print("Stopped by user")
+                GPIO.cleanup()
+        
+
